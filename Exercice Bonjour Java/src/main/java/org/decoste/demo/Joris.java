4ccdcdad7ac7d163c49a3a6f10b60ff123463f42
Nathaniel <1735103@cegepmontpetit.ca>
2021-08-24 10:44:01 -0400

    FCT : Exercice Bonjour Java

---
+++ Exercice Bonjour Java/src/main/java/org/decoste/demo/Joris.java
@@ -0,0 +1,9 @@
+package org.decoste.demo;
+
+public class Joris
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Salut Joris");
+    }
+}
---
+++ Exercice Bonjour Java/src/main/java/org/decoste/demo/decoste.java
@@ -0,0 +1,9 @@
+package org.decoste.demo;
+
+public class decoste
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Bonjour Décoste");
+    }
+}
